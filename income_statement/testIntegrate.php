4acb09482573d93a7eeaffd59e4a5db6360fa3cc
ywlin <yuwenlin@kkbox.com>
2015-01-26 01:57:29 +0800

    Rename key

--- income_statement/testIntegrate.php
+++ income_statement/testIntegrate.php
@@ -40,7 +40,7 @@ class DeconstructureIncomeStatement
     public $nominalList = [
         'code', //'公司代號',
         'title', //'公司名稱',
-        'operatingIncome', //'營業收入',
+        'operatingRevenue', //'營業收入',
         'costOfGoodsSold', //'營業成本': 為了賺取營業收入而產生的銷收產品或提供勞務成本
         'operatingGross', //'營業毛利(毛損)',
         '未實現銷貨(損)益',
@@ -48,7 +48,7 @@ class DeconstructureIncomeStatement
         '營業毛利(毛損)淨額',
         'operatingExpense', //'營業費用':其他與經常營業相關的費用，如 銷售 廣告 管理 總務 研發等
         '其他收益及費損淨額',
-        '營業利益(損失)',
+        'operatingProfits', //'營業利益(損失)',
         '營業外收入及支出',
         '稅前淨利(淨損)',
         '所得稅費用(利益)',
@@ -65,7 +65,7 @@ class DeconstructureIncomeStatement
         '綜合損益總額歸屬於母公司業主',
         '綜合損益總額歸屬於共同控制下前手權益',
         '綜合損益總額歸屬於非控制權益',
-        '基本每股盈餘(元)'
+        'EPS', //'基本每股盈餘(元)'
     ];
 
     public $raw;
