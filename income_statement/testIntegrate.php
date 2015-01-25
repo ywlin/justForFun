1a1d75814aeb5532f0dd38d2c6a2ee364536f419
ywlin <yuwenlin@kkbox.com>
2015-01-26 00:24:38 +0800

    Init DeconstructureIncomeStatement

--- income_statement/testIntegrate.php
+++ income_statement/testIntegrate.php
@@ -2,3 +2,12 @@
 
 $file_contents = file_get_contents('102-1.csv');
 var_dump($file_contents);
+class DeconstructureIncomeStatement
+{
+
+    public $raw;
+    public function __construct(array $raw)
+    {
+        $this->raw = $raw;
+    }
+}
